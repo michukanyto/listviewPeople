aa922885408da5483b779d8c339e5cc0b83bfa90
Marlon <escobar.marlon@gmail.com>
2019-03-21 13:51:07 -0400

    UI creation

--- app/src/main/java/com/lasalle/listofpeople/MainActivity.java
+++ app/src/main/java/com/lasalle/listofpeople/MainActivity.java
@@ -1,13 +1,21 @@
 package com.lasalle.listofpeople;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+        
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
