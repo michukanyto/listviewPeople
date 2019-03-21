cc97e99d371e1a0154a133b534aafd254c3b93df
Marlon <escobar.marlon@gmail.com>
2019-03-21 12:11:44 -0400

    Model classes creation

---
+++ app/src/main/java/model/Employee.java
@@ -0,0 +1,47 @@
+package model;
+
+public class Employee extends Person {
+    private  String empolyeeId;
+    private String job;
+    private int salary;
+
+    public Employee(String name, int age, String empolyeeId, String job, int salary) {
+        super(name, age);
+        this.empolyeeId = empolyeeId;
+        this.job = job;
+        this.salary = salary;
+    }
+
+    public String getEmpolyeeId() {
+        return empolyeeId;
+    }
+
+    public void setEmpolyeeId(String empolyeeId) {
+        this.empolyeeId = empolyeeId;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "empolyeeId='" + empolyeeId + '\'' +
+                ", job='" + job + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ app/src/main/java/model/Student.java
@@ -0,0 +1,37 @@
+package model;
+
+public class Student extends Person {
+    private String studentId;
+    private String program;
+
+
+    public Student(String name, int age, String studentId, String program) {
+        super(name, age);
+        this.studentId = studentId;
+        this.program = program;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getProgram() {
+        return program;
+    }
+
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "studentId='" + studentId + '\'' +
+                ", program='" + program + '\'' +
+                '}';
+    }
+}
