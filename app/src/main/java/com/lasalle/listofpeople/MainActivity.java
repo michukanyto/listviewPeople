f3b251d8e55d9f5e36e0bd568ea6028ff8810d32
Marlon <escobar.marlon@gmail.com>
2019-03-25 21:35:31 -0400

    All button solving problem

--- app/src/main/java/com/lasalle/listofpeople/MainActivity.java
+++ app/src/main/java/com/lasalle/listofpeople/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -17,7 +18,8 @@ import model.Employee;
 import model.Person;
 import model.Student;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,
+        AdapterView.OnItemClickListener, AdapterView.OnItemLongClickListener,DialogInterface.OnClickListener {
     TextView textViewId;
     TextView textViewAge;
     TextView textViewJob;
@@ -33,6 +35,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     ArrayAdapter<Person> personArrayAdapter;
     ArrayAdapter<Employee> employeeArrayAdapter;
     ArrayAdapter<Student> studentArrayAdapter;
+    ListType listType;
+    final String BLANKSPACE = "";
+    boolean flag;
+
+
+    private enum ListType{
+        STUDENT,
+        EMPLOYEE,
+        PERSON;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -72,14 +84,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.buttonStudent:
                 studentArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listStudents);
                 listViewPeople.setAdapter(studentArrayAdapter);
+                listType = ListType.STUDENT;
                 break;
             case R.id.buttonEmployee:
                 employeeArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listEmployees);
                 listViewPeople.setAdapter(employeeArrayAdapter);
+                listType = ListType.EMPLOYEE;
                 break;
             case R.id.buttonAll:
                 personArrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,listPerson);
+                Log.i("List Size ===> ",String.valueOf(listPerson.size()));
                 listViewPeople.setAdapter(personArrayAdapter);
+                listType = ListType.PERSON;
+                flag = true;
                 break;
             default:
                 break;
@@ -97,6 +114,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listStudents.add(new Student("Emilie",28,"ad15555","Marketing"));
         listStudents.add(new Student("Nicole",35,"ad7865","Computer Science"));
         listPerson.addAll(listStudents);
+        Log.i("List Size ===> ",String.valueOf(listPerson.size()));
     }
 
 
@@ -108,33 +126,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         listEmployees.add(new Employee("Camila", 28, "tt15555", "Engineer", 95000));
         listEmployees.add(new Employee("Jaime", 35, "tt7865", "Carpenter", 70000));
         listPerson.addAll(listEmployees);
-
+        Log.i("List Size ===> ",String.valueOf(listPerson.size()));
     }
 
     private void cleanTextViews(){
-        textViewId.setText("");
-        textViewAge.setText("");
-        textViewProgram.setText("");
-        textViewId.setText("");
-        textViewJob.setText("");
-        textViewSalary.setText("");
+        textViewId.setText(BLANKSPACE);
+        textViewAge.setText(BLANKSPACE);
+        textViewProgram.setText(BLANKSPACE);
+        textViewId.setText(BLANKSPACE);
+        textViewJob.setText(BLANKSPACE);
+        textViewSalary.setText(BLANKSPACE);
     }
 
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Log.i("------------->",parent.getAdapter().getItem(position).getClass().toString());
-        if (parent.getAdapter().getItem(position).getClass().equals(Student.class)){
-            textViewId.setText(listStudents.get(position).getStudentId());
-            textViewAge.setText(String.valueOf(listStudents.get(position).getAge()));
-            textViewProgram.setText(listStudents.get(position).getProgram());
-        }else if (parent.getAdapter().getItem(position).getClass().equals(Employee.class)){
-            textViewId.setText(listEmployees.get(position).getEmpolyeeId());
-            textViewAge.setText(String.valueOf(listEmployees.get(position).getAge()));
-            textViewJob.setText(listEmployees.get(position).getJob());
-            textViewSalary.setText(String.valueOf(listEmployees.get(position).getSalary()));
-        }
+        Log.i("------------->", parent.getAdapter().getItem(position).getClass().toString());
+        Log.i("------------->", String.valueOf(parent.getAdapter().getCount()));
+        if (flag) {
+//            Person aPerson = parent.getAdapter().getItem(position);
+            if (parent.getAdapter().getItem(position) instanceof Student) {
+                Student aStudent =(Student)listPerson.get(position);
+                textViewId.setText(aStudent.getStudentId());
+                textViewAge.setText(String.valueOf(aStudent.getAge()));
+                textViewProgram.setText(aStudent.getProgram());
 
+            } else {
+                Employee anEmployee = (Employee)listPerson.get(position);
+                textViewId.setText(anEmployee.getEmpolyeeId());
+                textViewAge.setText(String.valueOf(anEmployee.getAge()));
+                textViewJob.setText(anEmployee.getJob());
+                textViewSalary.setText(String.valueOf(anEmployee.getSalary()));
+                textViewProgram.setText(BLANKSPACE);
+            }
+
+        } else {
+
+            if (parent.getAdapter().getItem(position) instanceof Student) {
+                textViewId.setText(listStudents.get(position).getStudentId());
+                textViewAge.setText(String.valueOf(listStudents.get(position).getAge()));
+                textViewProgram.setText(listStudents.get(position).getProgram());
+
+            } else {
+                textViewId.setText(listEmployees.get(position).getEmpolyeeId());
+                textViewAge.setText(String.valueOf(listEmployees.get(position).getAge()));
+                textViewJob.setText(listEmployees.get(position).getJob());
+                textViewSalary.setText(String.valueOf(listEmployees.get(position).getSalary()));
+            }
+
+        }
+//        flag = false;
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+        return false;
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
 
     }
+
 }
+
